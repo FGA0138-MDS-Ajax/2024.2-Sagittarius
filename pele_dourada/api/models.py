11c339bbefba69ab812dbfdcbfabb4010b0ce64e
Gabriel Flores <gabrielfcoelho19@gmail.com>
2025-01-18 13:14:28 -0300

    feat: criação do user, order e atualização do CRUD Co-authored-by: Vilmar Fagundes <vilmar160176@gmail.com>

--- pele_dourada/api/models.py
+++ pele_dourada/api/models.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv, find_dotenv
 import os
+from datetime import datetime
 load_dotenv(find_dotenv())
 
 # conexao cliente servidor
@@ -9,7 +10,26 @@ connection_string = f'mongodb+srv://testeuser:TesteUser@teste00.sxs09.mongodb.ne
 client = MongoClient(connection_string)
 db = client.testeDB
 
-# Create your models here.
+#gerador de id para pedidos
+class PedidoIDGenerator:
+    contador = 0  # Inicializa o contador no início
+
+    @classmethod
+    def gerar_codigo_pedido(cls):
+        cls.contador += 1
+        data = datetime.now().strftime('%d%m%Y')  # DiaMesAno
+        return f"{data}-{cls.contador:06d}"  # Exemplo: 20250118-000001
+
+# modelo de documentos.
+class User():
+    def __init__(self, username, pwd):
+        self.username = username
+        self.pwd = pwd
+
+    def __dict__(self):
+        return {'username' : self.username,
+                'password' : self.pwd}
+    
 class Product():
     def __init__(self, name, price, qtd):
         self.name = name
@@ -17,32 +37,84 @@ class Product():
         self.qtd = qtd
 
     def __dict__(self):
-        return {'nome' : self.name,
-                'preço' : self.price,
+        return {'name' : self.name,
+                'price' : self.price,
                 'qtd' : self.qtd}
     
-class Pedido():
-    pass
+class Order(PedidoIDGenerator):
+    def __init__(self, products):
+        self.number = self.gerar_codigo_pedido()
+        self.products = products
 
-# collection
-estoque_collection = db.estoque
+    def total_price(self):
+        order_price = 0
+        for products in self.products:
+            order_price += products['price']
+        return order_price
+    
+    def __dict__(self):
+        return{'number' : self.number, 
+               'products' : self.products,
+               'total' : self.total_price()}
+
+# collections
+stock_collection = db.stock
+order_collection = db.order
+user_collection = db.user
 
 # CRUD
-produto = Product('batata', 5, 100)
+produto1 = Product('batata', 5, 2)
+produto2 = Product('frango', 10, 1)
+pedido = Order([produto1.__dict__(),produto2.__dict__()])
+user = User('testuser', 'testpwd')
 
+# criar documentos
 def insert_doc(collection, doc):
-    return collection.insert_one(doc.__dict__())
-
-def get_doc(collection, doc):
-    return collection.find_one({'nome' : doc.name})
-
-def update_produto(name, new_name=None, new_price=None, new_qtd=None):
-    update = {
-        '$set' : {'nome' : new_name, 'preço' : new_price, 'qtd' : new_qtd}
-    }
-    estoque_collection.update_one({'nome' : name}, update)
+    collection.insert_one(doc.__dict__())
     return
 
-def delete(collection, doc):
-    collection.delete_one({'nome' : doc.name})
+# ler documentos
+def get_user(doc):
+    return user_collection.find_one({'username' : doc.username})
+
+def get_product(doc):
+    return stock_collection.find_one({'name' : doc.name})
+
+def get_order(doc):
+    return order_collection.find_one({'number' : doc.number})
+
+# atualizar documentos
+def update_user(username, new_username=None, new_pwd=None):
+    update = {
+        '$set' : {'username' : new_username, 'password' : new_pwd}
+    }
+    stock_collection.update_one({'username' : username}, update)
+    return
+
+def update_product(name, new_name=None, new_price=None, new_qtd=None):
+    update = {
+        '$set' : {'name' : new_name, 'price' : new_price, 'qtd' : new_qtd}
+    }
+    stock_collection.update_one({'name' : name}, update)
+    return
+
+def update_order(number, products_list):
+    for i in range(0, len(products_list)):
+        update = {
+        '$set' : {'name' : products_list[i]['name'], 'price' : products_list[i]['price'], 'qtd' : products_list[i]['qtd']}
+        }
+        order_collection.update_one({'number' : number, 'products' : i}, update)
+    return
+
+#deletar documentos
+def delete_user(doc):
+    user_collection.delete_one({'username' : doc.username})
+    return
+
+def delete_product(doc):
+    stock_collection.delete_one({'name' : doc.name})
+    return
+
+def delete_order(doc):
+    order_collection.delete_one({'number' : doc.number})
     return
