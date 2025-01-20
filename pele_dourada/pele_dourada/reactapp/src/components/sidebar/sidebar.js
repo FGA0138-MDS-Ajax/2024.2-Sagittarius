fdd95b32864558483256c1b0f4da948bb6ef62d5
agrnegocio <andre.gr20@gmail.com>
2025-01-20 01:09:10 -0300

    sidebar icones direcionados

--- pele_dourada/pele_dourada/reactapp/src/components/sidebar/sidebar.js
+++ pele_dourada/pele_dourada/reactapp/src/components/sidebar/sidebar.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom"; // Importando o Link do react-router-dom
 import "./sidebar.css";
-import Logo from '../../assets/icons/logo.svg';
+import Logo from "../../assets/icons/logo.svg";
 import Dashboard from "../../assets/icons/dashboard-icon.svg";
 import Vendas from "../../assets/icons/vendas-icon.svg";
 import Estoque from "../../assets/icons/estoque-icon.svg";
@@ -8,7 +9,6 @@ import Clientes from "../../assets/icons/clientes-icon.svg";
 import Profile from "../../assets/icons/personIcon.svg";
 import Logout from "../../assets/icons/logout-icon.svg";
 import ChevronLeftSvg from "../../assets/icons/chevron_left.svg";
-
 
 function Sidebar({ isCollapsed, setIsCollapsed }) {
   const toggleSidebar = () => {
@@ -18,9 +18,9 @@ function Sidebar({ isCollapsed, setIsCollapsed }) {
   return (
     <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
       <header className="sidebar-header">
-        <a href="#" className="header-logo">
+        <Link to="/" className="header-logo">
           <img src={Logo} alt="Logo Frango" />
-        </a>
+        </Link>
         <button className="toggler sidebar-toggler" onClick={toggleSidebar}>
           <span className="material-symbols-rounded">
             <img src={ChevronLeftSvg} alt="Chevron Right" />
@@ -32,67 +32,37 @@ function Sidebar({ isCollapsed, setIsCollapsed }) {
         {/* Navegação Principal */}
         <ul className="nav-list primary-nav">
           <li className="nav-item">
-            <a href="#" className="nav-link">
-              <img
-                src={Dashboard}
-                alt="Dashboard Icon"
-                className="nav-icon"
-              />
+            <Link to="/dashboard" className="nav-link">
+              <img src={Dashboard} alt="Dashboard Icon" className="nav-icon" />
               <span className="nav-label">Dashboard</span>
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">
-            <img
-                src={Vendas}
-                alt="Vendas Icon"
-                className="nav-icon"
-              />
+            <Link to="/vendas" className="nav-link">
+              <img src={Vendas} alt="Vendas Icon" className="nav-icon" />
               <span className="nav-label">Vendas</span>
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">
-            <img
-                src={Estoque}
-                alt="Estoque Icon"
-                className="nav-icon"
-              />
+            <Link to="/estoque" className="nav-link">
+              <img src={Estoque} alt="Estoque Icon" className="nav-icon" />
               <span className="nav-label">Estoque</span>
-            </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a href="#" className="nav-link">
-            <img
-                src={Clientes}
-                alt="Clientes Icon"
-                className="nav-icon"
-              />
+            <Link to="/clientes" className="nav-link">
+              <img src={Clientes} alt="Clientes Icon" className="nav-icon" />
               <span className="nav-label">Clientes</span>
-            </a>
+            </Link>
           </li>
         </ul>
         {/* Navegação Secundária */}
         <ul className="nav-list secondary-nav">
           <li className="nav-item">
-            <a href="#" className="nav-link">
-            <img
-                src={Profile}
-                alt="Profile Icon"
-                className="nav-icon"
-              />
-              <span className="nav-label">Perfil</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">
-            <img
-                src={Logout}
-                alt="Logout Icon"
-                className="nav-icon"
-              />
+            <Link to="/" className="nav-link">
+              <img src={Logout} alt="Logout Icon" className="nav-icon" />
               <span className="nav-label">Logout</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
