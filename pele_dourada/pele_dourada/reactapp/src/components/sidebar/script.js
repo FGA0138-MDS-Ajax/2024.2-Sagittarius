32077660ab338f54598977b28baa13043d89c334
agrnegocio <andre.gr20@gmail.com>
2025-01-20 01:14:42 -0300

    arquivos index, script, style e temp deletados

--- pele_dourada/pele_dourada/reactapp/src/components/sidebar/script.js
+++
@@ -1,8 +0,0 @@
-
-// const sidebar = document.querySelector(".sidebar");
-// const sidebarToggle = document.querySelector(".sidebar-toggler"):
-
-// // Toggle sidebar's collapsed state
-// sidebarToggler.addEventListener("click", () => {
-//     sidebar.classList.toggler("collapsed");
-// });
