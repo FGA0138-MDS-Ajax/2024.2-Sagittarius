b00812fac7c6bfa0ecaaa715be42c7aea4bc7601
Vilmar Jose Fagundes <165099836+VilmarFagundes@users.noreply.github.com>
2025-01-31 11:51:57 -0300

    feat: criação do crud de faturação e reorganização do código Co-authored-by: Gabriel Flores <gabrielfcoelho19@gmail.com>

--- pele_dourada/api/models.py
+++ pele_dourada/api/models.py
@@ -48,6 +48,7 @@ class Order(PedidoIDGenerator):
         self.tipe = tipe
         self.payment = payment
         self.products = products
+        self.confirm = False
         self.date = datetime.now().strftime('%H:%M:%S')
 
     def total_price(self):
@@ -63,8 +64,8 @@ class Order(PedidoIDGenerator):
                'total' : self.total_price(),
                'date' : self.date,
                'payment' : self.payment,
-               'products' : self.products}
-    
+               'products' : self.products,
+               'confirm' : self.confirm}
 
 class Client():
     def __init__(self, name, phone, address):
@@ -77,19 +78,43 @@ class Client():
                 'phone': self.phone,
                 'address': self.address}
 
+class Billing():
+    def __init__(self, orders):
+        self.orders = orders
+        self.date = datetime.strptime(datetime.now().date())
+    
+    def total_billing(self):
+        billing_price = 0
+        for orders in self.orders:
+            billing_price += orders['total']
+        return billing_price
+    
+    def to_dict(self):
+        return {'date' : self.date,
+                'orders' : self.orders,
+                'total' : self.total_billing()}
+
 # collections
 stock_collection = db.stock
 order_collection = db.order
 user_collection = db.user
 client_collection = db.client
+billing_collection = db.billing
 # CRUD
-user = User('admin', 'admin')
 # criar documentos
 def insert_product(doc):
     if(get_product(doc.name) == None):
         stock_collection.insert_one(doc.to_dict())
         return
     return print('produto ja existe')
+
+def insert_client(doc):
+    if get_client(doc.name) is None:  # Verifica se o cliente já existe pelo nome
+        client_collection.insert_one(doc.to_dict())
+        print(f"Cliente {doc.name} inserido com sucesso!")
+    else:
+        print(f"Cliente {doc.name} já existe!")
+    return
 
 def insert_order(doc):
     if(get_product(doc.number) == None):
@@ -103,9 +128,19 @@ def insert_user(doc):
         return
     return print('produto ja existe')
 
+def insert_billing(doc):
+    billing_collection.insert_one(doc.to_dict())
+    return
+
 # ler documentos
 def get_user(username):
     return user_collection.find_one({'username' : username})
+
+def get_client(name):
+    return client_collection.find_one({'name': name})
+
+def get_all_clients():
+    return list(client_collection.find())
 
 def get_product(name):
     return stock_collection.find_one({'name' : name})
@@ -119,6 +154,9 @@ def get_order(number):
 def get_all_orders():
     return list(order_collection.find())
 
+def get_all_billing():
+    return list(billing_collection.find())
+
 # atualizar documentos
 def update_user(username, new_username=None, new_pwd=None):
     update = {
@@ -129,6 +167,21 @@ def update_user(username, new_username=None, new_pwd=None):
     if new_pwd is not None:
         update['$set']['password'] = new_pwd
     user_collection.update_one({'username' : username}, update)
+    return
+
+def update_client(name, new_name=None, new_phone=None, new_address=None):
+    update = {'$set': {}}
+    if new_name is not None:
+        update['$set']['name'] = new_name
+    if new_phone is not None:
+        update['$set']['phone'] = new_phone
+    if new_address is not None:
+        update['$set']['address'] = new_address
+    result = client_collection.update_one({'name': name}, update)
+    if result.modified_count > 0:
+        print(f"Cliente {name} atualizado com sucesso!")
+    else:
+        print(f"Nenhuma atualização feita para o cliente {name}.")
     return
 
 def update_product(name, new_name=None, new_price=None, new_qtd=None):
@@ -144,7 +197,7 @@ def update_product(name, new_name=None, new_price=None, new_qtd=None):
     stock_collection.update_one({'name' : name}, update)
     return
 
-def update_order(number, index=None, new_product=None, new_price=None, new_qtd=None, new_name=None, new_tipe=None, new_payment=None):
+def update_order(number, index=None, new_product=None, new_price=None, new_qtd=None, new_name=None, new_tipe=None, new_payment=None, new_confirm=None):
     update = {
         '$set': {}
     }
@@ -161,6 +214,8 @@ def update_order(number, index=None, new_product=None, new_price=None, new_qtd=N
         update['$set']['tipe'] = new_tipe
     if new_payment is not None:
         update['$set']['payment'] = new_payment
+    if new_confirm is not None:
+        update['$set']['confirm'] = new_confirm
      
     order_collection.update_one({'number': number}, update)
     return
@@ -168,6 +223,14 @@ def update_order(number, index=None, new_product=None, new_price=None, new_qtd=N
 #deletar documentos
 def delete_user(username):
     user_collection.delete_one({'username' : username})
+    return
+
+def delete_client(name):
+    result = client_collection.delete_one({'name': name})
+    if result.deleted_count > 0:
+        print(f"Cliente {name} deletado com sucesso!")
+    else:
+        print(f"Cliente {name} não encontrado.")
     return
 
 def delete_product(name):
@@ -178,67 +241,8 @@ def delete_order(number):
     order_collection.delete_one({'number' : number})
     return
 
-insert_user(user)
-
-
-# Criar Cliente
-def insert_client(doc):
-    if get_client(doc.name) is None:  # Verifica se o cliente já existe pelo nome
-        client_collection.insert_one(doc.to_dict())
-        print(f"Cliente {doc.name} inserido com sucesso!")
-    else:
-        print(f"Cliente {doc.name} já existe!")
-
-# Ler Cliente
-def get_client(name):
-    return client_collection.find_one({'name': name})
-
-# Ler Todos os Clientes
-def get_all_clients():
-    return list(client_collection.find())
-
-# Atualizar Cliente
-def update_client(name, new_name=None, new_phone=None, new_address=None):
-    update = {'$set': {}}
-    if new_name is not None:
-        update['$set']['name'] = new_name
-    if new_phone is not None:
-        update['$set']['phone'] = new_phone
-    if new_address is not None:
-        update['$set']['address'] = new_address
-    
-    result = client_collection.update_one({'name': name}, update)
-    if result.modified_count > 0:
-        print(f"Cliente {name} atualizado com sucesso!")
-    else:
-        print(f"Nenhuma atualização feita para o cliente {name}.")
-
-# Deletar Cliente
-def delete_client(name):
-    result = client_collection.delete_one({'name': name})
-    if result.deleted_count > 0:
-        print(f"Cliente {name} deletado com sucesso!")
-    else:
-        print(f"Cliente {name} não encontrado.")
-
-# Testando 
-
-# Registrar um Cliente
-new_client = client(name="João Paulo", phone="123456789", address="Rua A, 123")
-insert_client(new_client)
-
-# Ler um Cliente
-client = get_client("João Paulo")
-print(client)
-
-# Atualizar um Cliente
-update_client("João Paulo", new_phone="987654321", new_address="Rua B, 456")
-
-# Ler todos os Clientes
-clients = get_all_clients()
-print(clients)
-
-# Deletar um Cliente
-delete_client("João Paulo")
+def delete_billing(number):
+    billing_collection.delete_one({'number' : number})
+    return
 
 
