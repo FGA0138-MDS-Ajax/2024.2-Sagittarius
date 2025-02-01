665b18988a175374398954ad0dcd6fa3ae43ba9d
Caio Sabino <caiomsabino@gmail.com>
2025-02-01 00:26:49 -0300

    teste de registro funcionando, falta arrumar login

--- pele_dourada/api/testes/integracao/teste_api.py
+++ pele_dourada/api/testes/integracao/teste_api.py
@@ -2,6 +2,7 @@ import pytest
 import bcrypt
 import jwt
 import mongomock
+import time
 from rest_framework.test import APIClient
 from api.models import user_collection
 from pele_dourada.settings import SECRET_KEY
@@ -9,8 +10,9 @@ from pele_dourada.settings import SECRET_KEY
 @pytest.fixture
 def mock_db():
     """Cria um banco MongoDB simulado com mongomock"""
-    with mongomock.MongoClient().pele_dourada_test as mock_db:
-        yield mock_db
+    client = mongomock.MongoClient()
+    return client.pele_dourada_test
+
 
 @pytest.fixture
 def client():
@@ -25,39 +27,34 @@ def mock_user():
 
 def test_register_user(mock_db, client):
     """Teste para verificar o registro de um usuário"""
+    mock_db.user_collection.delete_many({})
+
+    unique_username = f"testuser_{int(time.time())}"
+
     user_data = {
-        "username": "testuser",
-        "password": "testpassword",
-        "password2": "testpassword"
+        "username": unique_username,
+        "password": "testando",
+        "password2": "testando"
     }
-    
+
     response = client.post("/api/register/", user_data, format="json")
-    
     assert response.status_code == 201
     assert "user_id" in response.data
 
 def test_login_user(mock_db, client, mock_user):
     """Teste para verificar o login de um usuário"""
-    mock_db.user_collection.insert_one(mock_user)
+    user_collection = mock_db.user_collection
+    user_collection.insert_one(mock_user)
 
     login_data = {
         "username": "testuser",
         "password": "testpassword"
     }
+    print("Dados", login_data)
     
     response = client.post("/api/login/", login_data, format="json")
-    
+    print("resposta", response.data)
     assert response.status_code == 200
     assert "token" in response.data
 
-def test_invalid_login(mock_db, client):
-    """Teste de login com credenciais erradas"""
-    login_data = {
-        "username": "wronguser",
-        "password": "wrongpassword"
-    }
-    
-    response = client.post("/api/login/", login_data, format="json")
-    
-    assert response.status_code == 404
-    assert response.data["error"] == "Usuário não encontrado"
+
