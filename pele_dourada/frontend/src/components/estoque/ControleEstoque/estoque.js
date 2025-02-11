1e15908cf808a12da489bae0fd3120f7371a4f52
lucas <lcsgborges@gmail.com>
2025-02-11 01:21:15 -0300

    fix: crud estoque

--- pele_dourada/frontend/src/components/estoque/ControleEstoque/estoque.js
+++ pele_dourada/frontend/src/components/estoque/ControleEstoque/estoque.js
@@ -6,14 +6,15 @@ import Sidebar from '../../../components/sidebar/sidebar';
 import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import { BsFillBoxSeamFill } from "react-icons/bs";
 
-
 function ControleEstoque() {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [produtos, setProdutos] = useState([]);
   const [busca, setBusca] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
   const [produtoEditando, setProdutoEditando] = useState(null);
+  const [produtoRemovendo, setProdutoRemovendo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
 
@@ -70,7 +71,7 @@ function ControleEstoque() {
 
   const handleEditProduct = async (produto) => {
     try {
-      const response = await axios.post('http://localhost:8000/api/product/update/', produto);
+      const response = await axios.put('http://localhost:8000/api/product/update/', produto);
       alert(response.data);
       setIsEditModalOpen(false);
       setProdutoEditando(null);
@@ -84,17 +85,21 @@ function ControleEstoque() {
     }
   };
 
-  const handleRemoveProduct = async (produto) => {
+  const handleRemoveProduct = async () => {
     try {
-      await axios.post('http://localhost:8000/api/product/delete/', {
-        name: produto.name,
+      await axios.delete('http://localhost:8000/api/product/delete/', {
+        data: { name: produtoRemovendo.name }
       });
-      window.location.reload();
+
+      setProdutos(produtos.filter((p) => p.name !== produtoRemovendo.name));
+      setIsConfirmModalOpen(false);
+      setProdutoRemovendo(null);
+      
     } catch (error) {
       console.error("Erro ao deletar produto:", error);
       alert('Erro ao deletar o produto');
     }
-  };
+};
 
   const formatCurrency = (value) => {
     if (!value) return "R$ 0,00";
@@ -131,6 +136,13 @@ function ControleEstoque() {
     );
   }
 
+  const handleAddProduct = (novoProduto) => {
+    setProdutos([...produtos, novoProduto]);
+    setIsModalOpen(false);
+  };
+  
+  <AdicionarProduto onAddProduct={handleAddProduct} />
+  
 
   return (
     <div className={`app-container ${isCollapsed ? "collapsed" : ""}`}>
@@ -207,7 +219,10 @@ function ControleEstoque() {
                         </button>
                         <button
                           className='controle-estoque-remove-button'
-                          onClick={() => handleRemoveProduct(produto)}>
+                          onClick={() => {
+                            setProdutoRemovendo(produto);
+                            setIsConfirmModalOpen(true);
+                          }}>
                           <FaTimes className="icon-button" /> Remover
                         </button>
                       </div>
@@ -224,7 +239,7 @@ function ControleEstoque() {
                 <button className="close-modal" onClick={() => setIsModalOpen(false)}>
                   &times;
                 </button>
-                <AdicionarProduto />
+                <AdicionarProduto onAddProduct={handleAddProduct} />
               </div>
             </div>
           )}
@@ -296,6 +311,23 @@ function ControleEstoque() {
             </div>
           )}
 
+          {isConfirmModalOpen && (
+            <div className="modal-overlay" onClick={() => setIsConfirmModalOpen(false)}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <h2>Confirmar Remoção</h2>
+                <p>Tem certeza que deseja remover o produto "{produtoRemovendo?.name}"?</p>
+                <div className="div-editar-produto-button">
+                  <button onClick={handleRemoveProduct} className="editar-produto-button">
+                    Confirmar
+                  </button>
+                  <button onClick={() => setIsConfirmModalOpen(false)} className="editar-produto-button">
+                    Cancelar
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
         </div>
       </main>
     </div>
@@ -303,134 +335,3 @@ function ControleEstoque() {
 }
 
 export default ControleEstoque;
-
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';  // Importe o Axios
-// import './controle_estoque.css'; 
-// import AdicionarProduto from '../AdicionarProduto/adicionar_produto';
-// import Sidebar from '../../../components/sidebar/sidebar';
-
-// function ControleEstoque() {
-//   const [produtos, setProdutos] = useState([]);
-//   const [busca, setBusca] = useState('');
-//   const [isModalOpen, setIsModalOpen] = useState(false); // Controle do modal
-//   const [isLoading, setIsLoading] = useState(true); // Para mostrar um carregando
-
-//   useEffect(() => {
-//     // Função para buscar os produtos da API
-//     const fetchProdutos = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:8000/api/products'); // Substitua pela URL correta da sua API
-//         setProdutos(response.data.products);
-//       } catch (error) {
-//         console.error("Erro ao buscar produtos:", error);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchProdutos();
-//   }, []);
-
-//   const handleBuscaChange = (e) => {
-//     setBusca(e.target.value);
-//   };
-
-//   const produtosFiltrados = produtos.filter((produto) =>
-//     produto.name.toLowerCase().includes(busca.toLowerCase())
-//   );
-
-//   const [isCollapsed, setIsCollapsed] = useState(false);
-
-//   return (
-//     <div className={`app-container ${isCollapsed ? "collapsed" : ""}`}>
-//       <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-//       <main className="main-content">
-//         <div className="controle-estoque-page" id="controle-estoque-page">
-//           <div className="controle-estoque-title" id="controle-estoque-title">
-//             <h1>Controle de Estoque</h1>
-//           </div>
-
-//           <div className='div-header-widgets'>
-//             <div className="controle-estoque-search" id="controle-estoque-search">
-//               <input
-//                 className="controle-estoque-input"
-//                 type="text"
-//                 placeholder="Buscar produto"
-//                 value={busca}
-//                 onChange={handleBuscaChange}
-//               />
-//             </div>
-
-//             <div className="controle-estoque-add-button" id="controle-estoque-add-button">
-//               <button 
-//                 className="controle-estoque-button" 
-//                 id="controle-estoque-button" 
-//                 onClick={() => setIsModalOpen(true)} // Abre o modal
-//               >
-//                 <i className="fas fa-plus"></i> 
-//                 Adicionar Produto
-//               </button>
-//             </div>
-//           </div>
-
-//           {isLoading ? (
-//             <div>Carregando...</div> // Exibe um texto de carregamento enquanto os dados não chegam
-//           ) : (
-//             <div className="controle-estoque-list" id="controle-estoque-list">
-//               <h2 className="controle-estoque-subtitle" id="controle-estoque-subtitle">Produtos no Estoque</h2>
-//               <table className="controle-estoque-table">
-//                 <thead>
-//                   <tr>
-//                     <th>Produto</th>
-//                     <th>Preço</th>
-//                     <th>Quantidade</th>
-//                   </tr>
-//                 </thead>
-//                 <tbody>
-//                   {produtosFiltrados.map((produto) => (
-//                     <tr key={produto.id}>
-//                       <td>{produto.name}</td>
-//                       <td>R$ {produto.price.toFixed(2)}</td>
-//                       <td>
-//                         <div className="quantidade-container"
-//                           style={{
-//                             backgroundColor: produto.qtd <= 20 ? 'red' :
-//                                               produto.qtd > 20 && produto.qtd <= 30 ? '#FFA600' : 
-//                                               '#74B816',
-//                             color: produto.qtd <= 20 ? 'white' :
-//                                    produto.qtd > 20 && produto.qtd <= 30 ? 'black' : 
-//                                    'white',
-//                             padding: '10px', 
-//                             borderRadius: '30px',
-//                           }}
-//                         >
-//                           {produto.qtd}
-//                         </div>
-//                       </td>
-//                     </tr>
-//                   ))}
-//                 </tbody>
-//               </table>
-//             </div>
-//           )}
-
-//           {/* Modal */}
-//           {isModalOpen && (
-//             <div className={`modal-overlay ${isModalOpen ? 'open' : ''}`} onClick={() => setIsModalOpen(false)}>
-//               <div className={`modal-content ${isModalOpen ? 'open' : ''}`} onClick={(e) => e.stopPropagation()}>
-//                 <button className="close-modal" onClick={() => setIsModalOpen(false)}>
-//                   &times;
-//                 </button>
-//                 <AdicionarProduto />
-//               </div>
-//             </div>
-//           )}
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default ControleEstoque;
